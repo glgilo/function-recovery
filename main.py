ff1bff3d0891e4004d19ac66d87d6d1aca6dd444
HelloWorld <michalon@post.bgu.ac.il>
2021-07-14 21:27:02 +0300

    Added description in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from averagesmoothnesslearner import *
 from utilities import *
 from tweet_experiment import *
+
+
 # [xtrain, ytrain], [xtest, ytest] = utilities.generate_experiment(0, 32)
 # print(xtrain, ytrain, xtest, ytest)
 # utilities.test_linear_program()
@@ -16,8 +18,8 @@ def plot_best_result():
         func = utilities.func_sign
 
     x = np.linspace(0, 1, 100)
-    plt.scatter(xtrain, ytrain, c='b', marker='x', label='Train sample')
-    plt.scatter(xtest, ytest, c='g', marker='s', label='Test sample')
+    # plt.scatter(xtrain, ytrain, c='b', marker='x', label='Train sample')
+    # plt.scatter(xtest, ytest, c='g', marker='s', label='Test sample')
     plt.plot(xtest, mlp_best_result['predictions'], c='g', label='mlp')
     plt.plot(xtest, krr_best_result['predictions'], c='m', label='krr')
     plt.plot(xtest, asl_best_result['predictions'], c='k', label='Average smoothness learner')
@@ -26,9 +28,9 @@ def plot_best_result():
     plt.ylabel('y - axis')
     # plt.title(' ASL n={}, L={} epsilon={}'.format(len(self.xs), self.L, self.epsilon))
     plt.title('n={}, ASL: L={} epsilon={}\nKRR: alpha = {}, MLP: alpha={}'.format(n, asl_best_result['L'],
-                                                                                 asl_best_result['epsilon'],
-                                                                                 krr_best_result['alpha'],
-                                                                                 mlp_best_result['alpha']))
+                                                                                  asl_best_result['epsilon'],
+                                                                                  krr_best_result['alpha'],
+                                                                                  mlp_best_result['alpha']))
     plt.legend()
     plt.show()
 
@@ -41,26 +43,31 @@ def compare_squared_loss():
 
 
 if __name__ == '__main__':
-    # demo_test_learner(n=32, L=10, epsilon=0.1, std_error=0.1, func_type=1)
-    # utilities.test_linear_program()
 
-    # set function as:
-    #   0 := sin(2*pi*x), n = 32
-    #   1 := sign(sin(2*pi*x)), n = 32
+    # In order to run the test, please:
+    #
+    #  1. set function as:
+    #     0 := sin(2*pi*x), n = 32
+    #     1 := sign(sin(2*pi*x)),
+    #
+    #  2. set n (default n=32)
+    #  3. set L values and lambda values
+
     func_type = 0
-    n = 32
+    n = 100
     epsilon = 0.1
 
     # Set variables values
-    L_values = np.arange(1, 11, 1)
-    regularisation_variables = np.arange(0, 0.0001, 0.000005)
+    L_values = np.arange(1, 21, 1)
+    regularisation_variables = np.arange(0, 0.0005, 0.000005)
 
     # generate sample
     [xtrain, ytrain], [xtest, ytest] = utilities.generate_experiment(func_type, n)
 
     # run experiments:
     asl_best_result = asl_experiment(xtrain, ytrain, xtest, ytest, L_values, epsilon, func_type)
-    mlp_best_result, krr_best_result = experiment_mlp_krr(xtrain, ytrain, xtest, ytest, regularisation_variables, func_type)
+    mlp_best_result, krr_best_result = experiment_mlp_krr(xtrain, ytrain, xtest, ytest, regularisation_variables,
+                                                          func_type)
 
     # print and plot results
     compare_squared_loss()
